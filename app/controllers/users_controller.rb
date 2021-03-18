cdfd4b6bcb6825d1e8d795e22a911d466f601349
yuki986 <yuki.kuribayashi8@gmail.com>
2021-03-18 14:27:05 +0900

    マイページの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      redirect_to "show"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
