0a4fc0d6c7ec1a13b9243f23f0cbd5c3ef6e4ec0
yuki986 <yuki.kuribayashi8@gmail.com>
2021-03-19 20:42:08 +0900

    マイページにクレカ情報を表示しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 環境変数を読み込む
+    card = Card.find_by(user_id: current_user.id)# ユーザーのid情報をもとにカード情報を取得
+
+    redirect_to new_card_path and return unless card.present?
+    
+    customer = Payjp::Customer.retrieve(card.customer_token) # 上のカード情報を元に、顧客情報を取得
+    @card = customer.cards.first
   end
 
   def update
